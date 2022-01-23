c3ac0105448da2fb3f3ba39bd2b5aef7ded6af53
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-22 19:20:02 -0500

    slightly changed database schema and temporarily removed transactions

--- install/install.php
+++ install/install.php
@@ -3,7 +3,7 @@ function exit_if_failure(PDOStatement $stmt)
 {
     if (!$stmt->execute()) {
         echo 'Installation failed!';
-        $GLOBALS["pdo"]->rollBack();
+//        $GLOBALS["pdo"]->rollBack();
         die();
     }
 }
@@ -39,48 +39,60 @@ $dsn = "mysql:host={$_POST['host']};dbname={$_POST['db-name']};charset=utf8mb4";
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_AUTOCOMMIT => false
 ];
-$pdo = null;
+
 try {
     $pdo = new PDO($dsn, $_POST['db-user'], $_POST['db-password'], $options);
 } catch (PDOException $e) {
     echo 'Error: ' . $e->getMessage();
+    die();
 }
 
-// Use a transaction so we don't have a partially completed operation
-$pdo->beginTransaction();
+try {
 
-// Create tables
-$stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_users` (
+    // Create tables
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_users` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL
 )");
-exit_if_failure($stmt);
-$stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_forums` (
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_forums` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL
 )");
-exit_if_failure($stmt);
-$stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_topics` (
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_topics` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
-  `forum_id` int
+  `content` text,
+  `forum_id` int,
+  `user_id` int
 )");
-exit_if_failure($stmt);
-$stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_posts` (
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_posts` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `content` text,
-  `topic_id` int
+  `topic_id` int,
+  `user_id` int
 )");
-exit_if_failure($stmt);
+    exit_if_failure($stmt);
 
 // Add foreign keys
-$stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_topics` ADD FOREIGN KEY (`forum_id`) REFERENCES `{$_POST['table-prefix']}_forums` (`id`)");
-exit_if_failure($stmt);
-
-$pdo->commit();
+    $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_topics` ADD FOREIGN KEY (`forum_id`) REFERENCES `{$_POST['table-prefix']}_forums` (`id`)");
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_topics` ADD FOREIGN KEY (`user_id`) REFERENCES `{$_POST['table-prefix']}_users` (`id`)");
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_posts` ADD FOREIGN KEY (`topic_id`) REFERENCES `{$_POST['table-prefix']}_topics` (`id`)");
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_posts` ADD FOREIGN KEY (`user_id`) REFERENCES `{$_POST['table-prefix']}_users` (`id`)");
+    exit_if_failure($stmt);
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+    die();
+}
 
 // Close database connection
 $pdo = null;

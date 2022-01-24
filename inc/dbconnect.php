86efb5c29831ac3cf90f35c3ceea40f4b113cc39
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-23 19:52:53 -0500

    db connect file now fully uses config file to create a db connection

--- inc/dbconnect.php
+++ inc/dbconnect.php
@@ -1,1 +1,23 @@
 <?php
+require_once 'config.php';
+
+$pdo = null;
+$host = DB_HOST;
+$dbname = DB_NAME;
+$user = DB_USER;
+$password = DB_PASSWORD;
+
+// Connect to the database
+$dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_AUTOCOMMIT => false
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+} catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}

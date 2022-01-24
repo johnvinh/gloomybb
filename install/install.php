dbce798653f87db6d79bebd28c29fd7ed8a1bfcd
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-23 20:08:43 -0500

    adjusted database schema and added categories table

--- install/install.php
+++ install/install.php
@@ -58,28 +58,36 @@ try {
   `password` varchar(255) NOT NULL
 )");
     exit_if_failure($stmt);
-    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_forums` (
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_categories` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL
+)");
+    exit_if_failure($stmt);
+    $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_forums` (
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `category_id` int
 )");
     exit_if_failure($stmt);
     $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_topics` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `title` varchar(30) NOT NULL,
-  `content` text,
-  `forum_id` int,
-  `user_id` int
+  `content` text NOT NULL,
+  `forum_id` int NOT NULL,
+  `user_id` int NOT NULL
 )");
     exit_if_failure($stmt);
     $stmt = $pdo->prepare("CREATE TABLE `{$_POST['table-prefix']}_posts` (
   `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `content` text,
-  `topic_id` int,
-  `user_id` int
+  `content` text NOT NULL,
+  `topic_id` int NOT NULL,
+  `user_id` int NOT NULL
 )");
     exit_if_failure($stmt);
 
 // Add foreign keys
+    $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_forums` ADD FOREIGN KEY (`category_id`) REFERENCES `{$_POST['table-prefix']}_categories` (`id`)");
+    exit_if_failure($stmt);
     $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_topics` ADD FOREIGN KEY (`forum_id`) REFERENCES `{$_POST['table-prefix']}_forums` (`id`)");
     exit_if_failure($stmt);
     $stmt = $pdo->prepare("ALTER TABLE `{$_POST['table-prefix']}_topics` ADD FOREIGN KEY (`user_id`) REFERENCES `{$_POST['table-prefix']}_users` (`id`)");
@@ -93,12 +101,14 @@ try {
     $password = password_hash($_POST['admin-password'], PASSWORD_DEFAULT);
     $stmt = $pdo->prepare("INSERT INTO {$_POST['table-prefix']}_users (username, password) VALUES (?, ?)");
     $stmt->execute([$_POST['admin-username'], $password]);
+    // Create a default category
+    $stmt = $pdo->prepare("INSERT INTO {$_POST['table-prefix']}_categories (name) VALUES ('My First Category')");
+    exit_if_failure($stmt);
     // Create a default forum
-    $stmt = $pdo->prepare("INSERT INTO {$_POST['table-prefix']}_forums (name) VALUES ('My First Forum')");
+    $stmt = $pdo->prepare("INSERT INTO {$_POST['table-prefix']}_forums (name, category_id) VALUES ('My First Forum', 1)");
     exit_if_failure($stmt);
     $pdo->commit();
 } catch (PDOException $e) {
-    $pdo->rollBack();
     throw new PDOException($e->getMessage(), (int)$e->getCode());
 }
 

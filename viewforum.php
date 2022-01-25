dfa0d482f3aa1d657f60c27ba85fa78601067d15
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-01-25 18:07:54 -0500

    new topic button on viewforum page now redirects to the new topic page

--- viewforum.php
+++ viewforum.php
@@ -51,4 +51,16 @@ $subforum_name->execute([$_GET['id']]);
     </main>
 </div>
 </body>
+<script type="text/javascript">
+    function getForumId()
+    {
+        const regex = new RegExp('^.+?\\?id=(.+)$');
+        return regex.exec(window.location.href)[1];
+    }
+
+    document.querySelector('#new-topic').addEventListener('click', (e) =>
+    {
+        window.location.href = `newtopic.php?forum_id=${getForumId()}`;
+    });
+</script>
 </html>

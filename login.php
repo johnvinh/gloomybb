d864d77d5cc817c302745d12bbb039367769ea0d
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-02 18:09:22 -0500

    login page now outputs the site template when there's an incorrect login instead of just the error

--- login.php
+++ login.php
@@ -5,6 +5,24 @@ require_once 'classes/Page.php';
 require_once 'inc/helpers.php';
 
 $content = "";
+// Nevigation
+$links = [
+    ['url' => 'index.php', 'name' => 'Index'],
+    ['url' => 'login.php', 'name' => 'Login']
+];
+$navigation = construct_navigation($links);
+// Main Page Content
+$content .= '<form action="login.php" method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
+            <input type="submit" name="login" value="Login!">
+        </form>';
 
 // Check if the user was referred from the viewtopic page
 if (isset($_SERVER['HTTP_REFERER']) && str_contains($_SERVER['HTTP_REFERER'], "viewtopic.php")) {
@@ -20,17 +38,23 @@ if (isset($_POST['login']) && $_POST['login'] === "Login!") {
     // Need to use empty() instead of isset() since clicking the button will always result
     // in these POST variables getting set
     if (empty($_POST['username'])) {
-        echo 'Missing username!';
+        $content = '<p>You need to enter a username!</p>' . $content;
+        $page = new Page("Login", $navigation, $content);
+        $page->write_html();
         die();
     }
     else if (empty($_POST['password'])) {
-        echo 'Missing password!';
+        $content = '<p>You need to enter a password!</p>' . $content;
+        $page = new Page("Login", $navigation, $content);
+        $page->write_html();
         die();
     }
 
     // Validate username
     if (!preg_match('/^[A-Za-z0-9]+?$/', $_POST['username'])) {
-        echo 'That username contains illegal characters.';
+        $content = '<p>That username contains illegal characters.</p>' . $content;
+        $page = new Page("Login", $navigation, $content);
+        $page->write_html();
         die();
     }
 
@@ -42,14 +66,18 @@ if (isset($_POST['login']) && $_POST['login'] === "Login!") {
     $stmt->execute([$_POST['username']]);
     // No users found
     if ($stmt->rowCount() === 0) {
-        echo 'No users found with that username found!';
+        $content = '<p>There are no users with that username.</p>' . $content;
+        $page = new Page("Login", $navigation, $content);
+        $page->write_html();
         die();
     }
     $results = $stmt->fetch();
     $hashed_password = $results['password'];
     // Incorrect password entered
     if (!password_verify($_POST['password'], $hashed_password)) {
-        echo 'Incorrect password!';
+        $content = '<p>That password is not correct.</p>' . $content;
+        $page = new Page("Login", $navigation, $content);
+        $page->write_html();
         die();
     }
     // Valid login, so we can start setting up the session
@@ -64,25 +92,6 @@ if (isset($_POST['login']) && $_POST['login'] === "Login!") {
 }
 
 $title = 'Login';
-// Nevigation
-$links = [
-    ['url' => 'index.php', 'name' => 'Index'],
-    ['url' => 'login.php', 'name' => 'Login']
-];
-$navigation = construct_navigation($links);
-
-// Main Page Content
-$content .= '<form action="login.php" method="post">
-            <div>
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username">
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password">
-            </div>
-            <input type="submit" name="login" value="Login!">
-        </form>';
 
 $page = new Page($title, $navigation, $content);
 $page->write_html();

65fc20d029756a3fbd048c9bfba3f4271761d2a4
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-03 11:33:56 -0500

    added check to ensure that only the user with id=1 can access the admin page

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,20 @@
 <?php
+require_once dirname(__FILE__, 2) . '/inc/config.php';
+require_once FORUM_ROOT . 'inc/dbconnect.php';
+require_once FORUM_ROOT . 'inc/helpers.php';
+require_once FORUM_ROOT . 'classes/Page.php';
+
+$navigation = construct_navigation([
+    ['url' => '../index.php', 'name' => 'Index']
+]);
+// Make sure the user is logged in
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ' . '../login.php');
+}
+// User needs to be the admin
+else if ($_SESSION['user_id'] !== 1) {
+    $page = new Page("Not Authorized", $navigation, "You are not authorized to view this page.");
+    $page->write_html();
+    die();
+}

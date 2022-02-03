35d3e5007c61d83d853e8d3efc1a2a723858b1ea
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-03 12:02:25 -0500

    new forums can now be added by the admin

---
+++ admin/addforum.php
@@ -0,0 +1,27 @@
+<?php
+require_once dirname(__FILE__, 2) . '/inc/config.php';
+require_once FORUM_ROOT . 'inc/dbconnect.php';
+
+// Ensure the submit button got clicked
+if (isset($_POST['submitting']) && $_POST['submitting'] === 'Add Forum') {
+    // Make sure forum name was entered
+    if (!isset($_POST['forum-name'])) {
+        die();
+    }
+    // Make sure category was entered
+    else if (!isset($_POST['category'])) {
+        die();
+    }
+
+    $pdo = get_pdo();
+    $table_prefix = TABLE_PREFIX;
+    $stmt = $pdo->prepare("INSERT INTO {$table_prefix}_forums (name, category_id) VALUES (?, ?)");
+    if ($stmt->execute([$_POST['forum-name'], $_POST['category']])) {
+        echo 'Forum successfully added!';
+        header('refresh:2;url=index.php');
+        $pdo->commit();
+    }
+    else {
+        $pdo->rollBack();
+    }
+}
--- admin/index.php
+++ admin/index.php
@@ -26,6 +26,24 @@ $content = '<form action="addcategory.php" method="post">
 </div>
 <input type="submit" name="submitting" value="Add Category">
 </form>
+<form action="addforum.php" method="post">
+<select name="category">
+';
+// Insert category options
+$table_prefix = TABLE_PREFIX;
+$pdo = get_pdo();
+$stmt = $pdo->prepare("SELECT * FROM {$table_prefix}_categories");
+$stmt->execute();
+foreach ($stmt as $row) {
+    $content .= '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
+}
+$content .= '</select>
+<div>
+<label for="forum-name">Forum Name</label>
+<input type="text" id="forum-name" name="forum-name">
+</div>
+<input type="submit" name="submitting" value="Add Forum">
+</form>
 ';
 $page = new Page("Admin Control Panel", $navigation, $content);
 $page->write_html();

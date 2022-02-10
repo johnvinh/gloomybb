a2f7f0215ff1cd2db4eecb576539cf8acd952f43
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-10 11:30:21 -0500

    replaced intrusive alerts for errors on login page with pretty on page errors

--- scripts/login.js
+++ scripts/login.js
@@ -1,14 +1,23 @@
 document.querySelector('input[name=login]').addEventListener('click', (e) =>
 {
+    const errorSpace = document.querySelector('main > div:first-child');
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
+    // Error stuff
+    const currentError = document.querySelector('.error');
+    if (currentError !== null)
+        currentError.parentElement.removeChild(currentError);
+    const error = document.createElement('p');
+    error.classList.add('error');
     if (username === '') {
         e.preventDefault();
-        alert("Please enter a username.");
+        error.innerHTML = 'Please enter a username.';
+        errorSpace.parentElement.insertBefore(error, errorSpace.nextSibling);
         return;
     } else if (password === '') {
         e.preventDefault();
-        alert("Please enter a password.");
+        error.innerHTML = 'Please enter a password.';
+        errorSpace.parentElement.insertBefore(error, errorSpace.nextSibling);
         return;
     }
     e.target.parentElement.submit();

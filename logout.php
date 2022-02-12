3c451f098eb5fa88d6a966c8933ced0ca26fe465
John Vinh <61337643+johnvinh@users.noreply.github.com>
2022-02-12 10:04:50 -0500

    made logout success message pretty instead of just being echoed

--- logout.php
+++ logout.php
@@ -1,6 +1,15 @@
 <?php
+require_once 'classes/Page.php';
+require_once 'inc/helpers.php';
+
 session_start();
 session_unset();
 session_destroy();
+
+$navigation = construct_navigation([
+    ['url' => 'index.php', 'name' => 'Index'],
+    ['url' => 'login.php', 'name' => 'Login']
+]);
+$page = new Page("Logout", $navigation, "<p>Logout successful!</p>");
+$page->write_html();
 header("refresh:2;url=index.php");
-echo 'Logout successful!';

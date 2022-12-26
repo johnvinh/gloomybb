e83f102948f2680f7b3248369adc466dac88125c
John Vinh <john.michael.vinh2@gmail.com>
2022-12-26 11:46:10 -0500

    Update post structure

--- viewtopic.php
+++ viewtopic.php
@@ -50,21 +50,23 @@ $navigation = construct_navigation($links);
 $content = '<input type="button" id="new-reply" value="New Reply">';
 // Topic contents
 $content .= '<div class="post">';
-
+// Make post date human-readable
+$post_date = new DateTime($result['posted_at']);
+$post_date = $post_date->format('F j, Y, g:i a');
 // User details
-$content .= '<dl>';
-$content .= '<dt>Username</dt>';
 $user_stmt = $pdo->prepare("SELECT * FROM {$table_prefix}_users WHERE id = ?");
 $user_stmt->execute([$result['user_id']]);
-$user_stmt = $user_stmt->fetch();
-$content .= '<dd>' . $user_stmt['username'] . '</dd>';
-$content .= '<dt>Posted At</dt>';
-$content .= '<dd>' . $result['posted_at'] . '</dd>';
-$content .= '<dt>User Joined</dt>';
-$content .= '<dd>' . $user_stmt['joined_at'] . '</dd>';
-$content .= '</dl>';
+$user_result = $user_stmt->fetch();
+// Make join date more readable
+$join_date = new DateTime($user_result['joined_at']);
+$join_date = $join_date->format('F j, Y');
+$content .= '<div class="user-details">';
+$content .= '<h3>' . htmlspecialchars($user_result['username']) . '</h3>';
+$content .= '<p>Joined: ' . $join_date . '</p>';
+$content .= '</div>';
 // Post details
 $content .= '<div class="post-content">';
+$content .= '<span class="post-date">' . $post_date . '</span>';
 // Delete button
 if ((isset($_SESSION['user_id']) && ($_SESSION['user_id'] === $result['user_id']))
     || (isset($_SESSION['user_id']) && $_SESSION['user_id'] === 1)) {
@@ -82,20 +84,23 @@ $posts_stmt = $pdo->prepare("SELECT * FROM {$table_prefix}_posts WHERE topic_id 
 $posts_stmt->execute([$_GET['id']]);
 foreach ($posts_stmt as $post) {
     $content .= '<div id="post' . $post['id'] . '" class="post">';
+    // Make post date human-readable
+    $date = new DateTime($post['posted_at']);
+    $date = $date->format('F j, Y, g:i a');
     // User details
-    $content .= '<dl>';
-    $content .= '<dt>Username</dt>';
     $user_stmt = $pdo->prepare("SELECT * FROM {$table_prefix}_users WHERE id = ?");
-    $user_stmt->execute([$post['user_id']]);
+    $user_stmt->execute([$result['user_id']]);
     $user_stmt = $user_stmt->fetch();
-    $content .= '<dd>' . $user_stmt['username'] . '</dd>';
-    $content .= '<dt>Posted At</dt>';
-    $content .= '<dd>' . $post['posted_at'] . '</dd>';
-    $content .= '<dt>User Joined</dt>';
-    $content .= '<dd>' . $user_stmt['joined_at'] . '</dd>';
-    $content .= '</dl>';
+    $join_date = new DateTime($user_stmt['joined_at']);
+    // Make join date more readable
+    $join_date = $join_date->format('F j, Y');
+    $content .= '<div class="user-details">';
+    $content .= '<h3>' . $user_stmt['username'] . '</h3>';
+    $content .= '<p>Joined: ' . $join_date . '</p>';
+    $content .= '</div>';
     // Post details
     $content .= '<div class="post-content">';
+    $content .= '<span class="post-date">' . $date . '</span>';
     // Delete button
     if ((isset($_SESSION['user_id']) && $_SESSION['user_id'] === $post['user_id'])
         || (isset($_SESSION['user_id']) && $_SESSION['user_id'] === 1)) {
@@ -111,5 +116,5 @@ foreach ($posts_stmt as $post) {
 $pdo = null;
 // End Main Content
 $script = 'scripts/viewtopic.js';
-$page = new Page($title, $navigation, $content, $script);
+$page = new Page($title, $navigation, $content, $script, "viewtopic.css");
 $page->write_html();
